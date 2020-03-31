b71da7c29885fbb8210b69ea70d4f77268823bc2
Norma Lopez <norma_l304@yahoo.com>
2020-03-31 09:08:29 -0600

    Addtest for name

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -8,4 +8,10 @@ class RenterTest < Minitest::Test
 
     assert_instance_of Renter, renter1
   end
+
+  def test_it_has_a_name
+    renter1 = Renter.new("Jessie")
+
+    assert_equal "Jessie", renter1.name 
+  end
 end
