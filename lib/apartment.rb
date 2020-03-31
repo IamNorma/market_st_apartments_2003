1e810ba33f5f82291be1bd969a0c17d01e85a624
Norma Lopez <norma_l304@yahoo.com>
2020-03-31 09:27:17 -0600

    Add method for add renter

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,10 +1,15 @@
 class Apartment
-  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
 
   def initialize(renter_info)
     @number = renter_info[:number]
     @monthly_rent = renter_info[:monthly_rent]
     @bathrooms = renter_info[:bathrooms]
     @bedrooms = renter_info[:bedrooms]
+    @renter = nil
+  end
+
+  def add_renter(renter)
+    @renter = renter 
   end
 end
